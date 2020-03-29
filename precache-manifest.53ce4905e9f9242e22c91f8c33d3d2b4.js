81b8033f80fc36c2283cdcd813314780fe561429
Arun Nehra <arun.nehra.007@gmail.com>
2020-03-29 10:13:26 +0530

    Updates

---
+++ precache-manifest.53ce4905e9f9242e22c91f8c33d3d2b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75380e2db69342aa73418248cfffc22",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d756d46dc2d8b5165e7d",
+    "url": "/monsters-rolodex/static/css/main.6ede8b9f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d756d46dc2d8b5165e7d",
+    "url": "/monsters-rolodex/static/js/main.6b1edbd8.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
